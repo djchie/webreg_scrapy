3fb9566e71e72e1409a16bc6805498db6815dcaa
Derrick J Chie <djchie.dev@gmail.com>
2015-10-13 22:05:31 -0700

    Removes print

--- webreg_scrapy/models.py
+++ webreg_scrapy/models.py
@@ -18,8 +18,6 @@ def db_connect():
     """
     environment = os.environ.get("SCRAPY_ENV")
     if (environment == 'production'):
-        print 'WHAT IS THE DATABASE URL???'
-        print settings.PRODUCTION_DATABASE
         return create_engine(settings.PRODUCTION_DATABASE)
     elif (environment == 'test'):
         return create_engine(URL(**settings.TEST_DATABASE))
